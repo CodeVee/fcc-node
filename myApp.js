7590c90d3e0446f22b97133436d2e5ddb38a4c1b
Victor Onyebuchi <victorchike247@gmail.com>
2021-06-11 08:38:46 +0100

    registered static middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ var app = express();
 
 console.log('Hello World');
 
+app.use('/public', express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
     const path = __dirname + '/views/index.html';
     res.sendFile(path);
