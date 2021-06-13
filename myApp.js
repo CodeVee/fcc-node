be8fc76e5e277e786bd2a9817ff39437bd3e0c79
Victor Onyebuchi <victorchike247@gmail.com>
2021-06-13 13:05:43 +0100

    added root logger

--- myApp.js
+++ myApp.js
@@ -4,7 +4,12 @@ var app = express();
 
 console.log('Hello World');
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.get('/', function (req, res) {
     const path = __dirname + '/views/index.html';
