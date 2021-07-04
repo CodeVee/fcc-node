eae5fe3271d488fd39b9e3b4900186b513a2062c
Victor Onyebuchi <victorchike247@gmail.com>
2021-07-04 23:24:49 +0100

    moved json endpoint

--- myApp.js
+++ myApp.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 
 
-app.get('/json', function (req, res) {
+app.get('/json', (req, res) => {
   let response = "Hello json";
   const secret = process.env.MESSAGE_STYLE;
   if(secret === "uppercase") response = response.toLocaleUpperCase();
